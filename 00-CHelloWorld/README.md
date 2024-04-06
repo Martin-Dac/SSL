e26439b78c60f3b98241f7dce2d64be6f6be47f4
MartinDac <martincunha.avila@gmail.com>
2024-04-06 18:17:06 -0300

    Hello world en C

---
+++ 00-CHelloWorld/README.md
@@ -0,0 +1,3 @@
+## Hello world en C
+
+Es un programa que hace un hello world en c que redirecciona a un .txt haciendo ./hello.exe > output.txt en consola
---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,14 @@
+/*
+    Hello.c
+    Es un Hello World en c
+    Martin Gabriel de Avila Cunha
+    06/04/2024
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+printf("Hello World!");
+
+}
